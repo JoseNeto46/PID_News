0a2e580e4abdea8c50a21d01e04493e15c2341fe
José Neto <83992705+JoseNeto46@users.noreply.github.com>
2021-07-27 23:12:02 -0300

    Ajustes BBC 2.0

--- noticias/teste.py
+++ noticias/teste.py
@@ -4,13 +4,13 @@ import pandas as pd
 
 lista_produtos = []
 response = requests.get('https://www.amazon.com.br/gp/goldbox/')
-
+print('1')
 content = response.content
-
+print('2')
 site = BeautifulSoup(content, 'html.parser')
 
-produtos = site.findAll('div', attrs={'aria-label': 'Grade de ofertas'})
-print('teste')
+produtos = site.findAll('div', attrs={'class': 'Grid-module__grid_1-xkdMK87Hfx0wjqVxAGcI'})
+print('3')
 for produto in produtos:
     nome = produto.find('div', attrs={'class': 'DealCard-module__truncate_3E_98PYsAQzbYk0BLscdkC'})
     print('teste2')
---
+++ noticias/teste2.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+list_produtos = []
+
+url_base = 'https://lista.mercadolivre.com.br/'
+
+produto_nome = input('Qual produto você deseja? ')
+
+response = requests.get(url_base + produto_nome)
+
+site = BeautifulSoup(response.text, 'html.parser')
+
+produtos = site.findAll('div', attrs={'class': 'andes-card andes-card--flat andes-card--default ui-search-result ui-search-result--core andes-card--padding-default'})
+
+for produto in produtos:
+    titulo = produto.find('h2', attrs={'class': 'ui-search-item__title'})
+
+    link = produto.find('a', attrs={'class': 'ui-search-link'})
+
+    real = produto.find('span', attrs={'class': 'price-tag-fraction'})
+    list_produtos.append([titulo.text, link['href'], real.text])
+
+    print(titulo.text)
+    print(link['href'])
+    print('R$', real.text)
+    print('\n\n')
+print(list_produtos)
